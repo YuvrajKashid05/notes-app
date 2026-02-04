8706e96fc985a5bada9fb72dd450130875e15fca
YuvrajKashid05 <yuvraj.kashid03@gmail.com>
2026-02-04 11:19:42 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
   origin: 'https://notes-app-orcin-zeta.vercel.app',
     credentials: true,
 }));
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('noteapp_token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("noteapp_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
